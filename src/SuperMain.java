bcc3bf5ef046d7551cd8ae36c278f69e05813338
vic3r <barcelonamane@hotmail.com>
2017-08-23 16:40:51 -0500

    v 0.0.1

--- src/SuperMain.java
+++ src/SuperMain.java
@@ -1,25 +1,40 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class SuperMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner in = new Scanner(System.in);
 		Payee payee = new Payee();
 		Purchases purchases = new Purchases();
-		System.out.println("How many purchases did you do today?");
-		int n = in.nextInt();
-		while(n-->0){
-			System.out.println("Total of your purchase: ");
-			purchases.addPurchase(in.nextInt());
-			System.out.println("Name of the payee: ");
-			payee.Add(in.next());
-		}
-		System.out.println("Min :"+ purchases.getMin());
-		System.out.println("Max :"+ purchases.getMax());
-		System.out.println("Avg :"+ purchases.getAvg());
-		System.out.println("Most Frequent :"+ payee.getFrequent());
 		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+		    int nLines = Integer.parseInt(br.readLine());
+		    while(nLines-->0){
+		    	purchases.addPurchase(Integer.parseInt(br.readLine()));
+		    	payee.Add(br.readLine());
+		    }
+		    br.close();  
+		}catch(NumberFormatException | IOException e){
+	    	e.printStackTrace();
+	    }
+		
+		try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+	              new FileOutputStream("Results.txt"), "utf-8"))) {
+			writer.write("Min :"+ purchases.getMin()+"\n");
+			writer.write("Max :"+ purchases.getMax()+"\n");
+			writer.write("Avg :"+ purchases.getAvg()+"\n");
+			writer.write("Most Frequent :"+ payee.getFrequent());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
